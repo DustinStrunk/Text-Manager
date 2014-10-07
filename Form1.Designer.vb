17c38ef8f4ca33883cad06696e947de860bd6929
DustinStrunk <dms6212@psu.edu>
2014-10-07 10:39:38 -0400

    Source Code
    Project I did a while ago, uses some bad techniques but overall, it
    functions.

---
+++ Form1.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TreeView1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TreeView1.Location = New System.Drawing.Point(2, 5)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(376, 384)
+        Me.TreeView1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(2, 5)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(376, 384)
+        Me.TextBox2.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(92, 395)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(202, 20)
+        Me.TextBox3.TabIndex = 13
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(12, 398)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(78, 13)
+        Me.Label12.TabIndex = 14
+        Me.Label12.Text = "Speech Name:"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(300, 393)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(78, 23)
+        Me.Button7.TabIndex = 13
+        Me.Button7.Text = "Save"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(633, 21)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(148, 20)
+        Me.TextBox4.TabIndex = 15
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(519, 24)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(108, 13)
+        Me.Label13.TabIndex = 16
+        Me.Label13.Text = "New Directory Name:"
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(478, 19)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(40, 22)
+        Me.Button8.TabIndex = 17
+        Me.Button8.Text = "Save"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(500, 395)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(40, 22)
+        Me.Button9.TabIndex = 20
+        Me.Button9.Text = "Save"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(541, 400)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(103, 13)
+        Me.Label14.TabIndex = 19
+        Me.Label14.Text = "New Speech Name:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(655, 397)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(148, 20)
+        Me.TextBox5.TabIndex = 18
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label11)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Location = New System.Drawing.Point(384, 56)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(397, 162)
+        Me.Panel1.TabIndex = 22
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(146, 55)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(167, 20)
+        Me.TextBox1.TabIndex = 33
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.3!)
+        Me.Label11.Location = New System.Drawing.Point(17, 55)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(123, 15)
+        Me.Label11.TabIndex = 32
+        Me.Label11.Text = "New Directory Name:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(103, 109)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(65, 13)
+        Me.Label8.TabIndex = 30
+        Me.Label8.Text = "00/00/0000"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(113, 82)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(91, 13)
+        Me.Label7.TabIndex = 29
+        Me.Label7.Text = "Select a Directory"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.3!)
+        Me.Label2.Location = New System.Drawing.Point(17, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 15)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "Creation Date:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.3!)
+        Me.Label1.Location = New System.Drawing.Point(17, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 15)
+        Me.Label1.TabIndex = 26
+        Me.Label1.Text = "Directory Name:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 23)
+        Me.Button1.TabIndex = 24
+        Me.Button1.Text = "Add New Directory"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(127, 13)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(104, 23)
+        Me.Button3.TabIndex = 25
+        Me.Button3.Text = "Delete Directory"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(237, 13)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(104, 23)
+        Me.Button4.TabIndex = 23
+        Me.Button4.Text = "Edit Directory"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Label10)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.Button6)
+        Me.Panel2.Location = New System.Drawing.Point(384, 240)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(395, 151)
+        Me.Panel2.TabIndex = 23
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(102, 90)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "00/00/0000"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(104, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Add New Speech"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(83, 63)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(84, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Select a speech"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.3!)
+        Me.Label5.Location = New System.Drawing.Point(15, 88)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(85, 15)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Creation Date:"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(126, 25)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(104, 23)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Delete Speech"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.3!)
+        Me.Label6.Location = New System.Drawing.Point(15, 61)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 15)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Speech:"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(236, 25)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(104, 23)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Edit Speech"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(384, 4)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(69, 20)
+        Me.Button10.TabIndex = 24
+        Me.Button10.Text = "Copy"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(384, 30)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(69, 20)
+        Me.Button11.TabIndex = 25
+        Me.Button11.Text = "Edit"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(384, 30)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(69, 20)
+        Me.Button12.TabIndex = 26
+        Me.Button12.Text = "Save"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(811, 423)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Canned Speech Manager"
+        Me.TransparencyKey = System.Drawing.Color.RosyBrown
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,286 @@
+﻿
+
+
+
+Imports System.Environment
+Imports System.IO
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    'Declarations 
+
+    Dim appData = GetFolderPath(SpecialFolder.ApplicationData)
+    Dim CSM = "\CS_Manager\"
+    Dim counter As Integer = 0
+    Dim treeParentText As String
+    Dim parentnode As String
+    Dim test As Boolean
+
+
+
+
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs)
+        Label14.Show()
+        Button9.Show()
+        TextBox5.Show()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Button12.Hide()
+        Button7.Enabled = False
+        TextBox3.Enabled = False
+        TextBox4.Hide()
+        Label13.Hide()
+        Button8.Hide()
+        TreeView1.Show()
+        TextBox2.Hide()
+        Label14.Hide()
+        Button9.Hide()
+        TextBox5.Hide()
+
+        ' see if the app data path exists
+        If Directory.Exists(appData + "\CS_Manager\") Then
+
+
+        Else
+            Directory.CreateDirectory(appData + "\CS_Manager\")
+
+
+
+
+        End If
+        fillTree()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs)
+
+
+        If TextBox1.Text.Length = 0 Or TextBox1.Text.Length > 20 Or TextBox1.Text.Contains(".") Then
+            MessageBox.Show("Please note, you must assign of value of 1 - 20 characters and they cann't conatin a backslash (\) or a dot (.)")
+        Else
+            For Each p In Directory.GetDirectories(appData + CSM)
+                If TextBox1.Text = p.Substring(p.LastIndexOf("\") + 1) Then
+                    MsgBox("There is already a directory named that!")
+                    Dim counter = 0
+
+                Else
+                    Directory.CreateDirectory(appData + CSM + TextBox1.Text)
+                End If
+            Next
+
+
+            fillTree()
+
+
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs)
+        Directory.Delete(appData + CSM + TreeView1.SelectedNode.Text)
+        TreeView1.Nodes.Remove(TreeView1.SelectedNode)
+    End Sub
+
+    Private Sub fillTree()
+        counter = 0
+        TreeView1.Nodes.Clear()
+        TreeView1.BeginUpdate()
+
+
+        For Each f In Directory.GetDirectories(appData + CSM)
+
+            TreeView1.Nodes.Add(f.Substring(f.LastIndexOf("\") + 1))
+
+
+
+
+        Next
+        Dim folders As String() = Directory.GetDirectories(appData + CSM)
+        For Each q In folders
+            For Each r In Directory.GetFiles(q)
+                Dim s As Integer = r.LastIndexOf("\") + 1
+                Dim p As Integer = r.LastIndexOf(".")
+                Dim z As Integer = p - s
+                TreeView1.Nodes(counter).Nodes.Add(r.Substring(s, z))
+            Next
+            counter = counter + 1
+        Next
+        counter = 0
+        TreeView1.EndUpdate()
+
+
+
+
+    End Sub
+
+
+    ''''''
+  
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs)
+        Dim treeViewText = TreeView1.SelectedNode.Text
+        TextBox2.Show()
+
+    End Sub
+
+    Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Button7.Click
+
+        Dim sw As StreamWriter
+        sw = New StreamWriter(appData + CSM + "\" + Label7.Text + "\" + TextBox3.Text + ".txt", False)
+        sw.Write(TextBox2.Text)
+        sw.Close()
+        TextBox2.Clear()
+        TextBox2.Hide()
+        fillTree()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs)
+        Label13.Show()
+        TextBox4.Show()
+        Button8.Show()
+    End Sub
+
+    Private Sub Button8_Click(sender As System.Object, e As System.EventArgs) Handles Button8.Click
+
+
+        If Label7.Text = "Select a Directory" Then
+            MsgBox("Please select a directory!")
+        Else
+            My.Computer.FileSystem.RenameDirectory(appData + CSM + "\" + Label7.Text, TextBox4.Text)
+
+            fillTree()
+        End If
+        Button8.Hide()
+        Label13.Hide()
+        TextBox4.Hide()
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs)
+        If test = False Then
+            MsgBox("Please select a speech, not a directory!")
+        Else
+            File.Delete(appData + CSM + parentnode + "\" + TreeView1.SelectedNode.Text + ".txt")
+
+            fillTree()
+        End If
+
+    End Sub
+
+    Private Sub treeView1_AfterSelect(sender As Object, _
+  e As TreeViewEventArgs) Handles TreeView1.AfterSelect 
+        If (e.Node.Parent IsNot Nothing) Then
+            If (e.Node.Parent.GetType() Is GetType(TreeNode)) Then
+                parentnode = e.Node.Parent.Text
+                treeParentText = e.Node.Parent.Text
+                Label10.Text = e.Node.Text
+                Label7.Text = e.Node.Parent.Text
+                test = True
+                Label4.Text = File.GetCreationTime(appData + CSM + e.Node.Parent.Text + "\" + e.Node.Text + ".txt")
+                Dim nodeText = e.Node.Text
+            End If
+        Else
+            Label8.Text = Directory.GetCreationTime(appData + CSM + e.Node.Text)
+            Label7.Text = e.Node.Text
+            test = False
+
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As System.Object, e As System.EventArgs) Handles Button9.Click
+
+        My.Computer.FileSystem.RenameFile(appData + CSM + parentnode + "\" + Label10.Text + ".txt", TextBox5.Text + ".txt")
+
+        fillTree()
+    End Sub
+
+   
+    Private Sub Button10_Click(sender As System.Object, e As System.EventArgs) Handles Button10.Click
+        If test = False Then
+            MsgBox("Please select a directory!")
+        Else
+            My.Computer.Clipboard.Clear()
+            Dim path As String = appData + CSM + Label7.Text + "\" + Label10.Text + ".txt"
+            Dim sr As StreamReader = New StreamReader(path)
+            My.Computer.Clipboard.SetText(sr.ReadToEnd)
+            sr.Close()
+        End If
+    End Sub
+
+    Private Sub Button2_Click_1(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        TextBox3.Enabled = True : Button7.Enabled = True : TextBox2.Show()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If Directory.Exists(appData + CSM + TextBox1.Text) = True Then
+            MsgBox("Sorry that is already a directory!")
+        End If
+        Directory.CreateDirectory(appData + CSM + TextBox1.Text)
+        fillTree()
+    End Sub
+
+    Private Sub Button3_Click_1(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+
+        Try
+            Directory.Delete(appData + CSM + Label7.Text, True)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        fillTree()
+    End Sub
+
+    Private Sub Button5_Click_1(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Try
+            File.Delete(appData + CSM + Label7.Text + "\" + Label10.Text + ".txt")
+        Catch ex As Exception
+
+        End Try
+        fillTree()
+    End Sub
+
+    Private Sub Button6_Click_1(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        Button9.Show()
+        Label14.Show()
+        TextBox5.Show()
+    End Sub
+
+    Private Sub Button4_Click_1(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        If test = False Then
+            Button8.Show()
+            Label13.Show()
+            TextBox4.Show()
+        Else
+            MsgBox("Please select a directory!")
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As System.Object, e As System.EventArgs) Handles Button11.Click
+        Button12.Show()
+        Dim rd As String = appData + CSM + "\" + Label7.Text + "\" + Label10.Text + ".txt"
+        Dim sr As StreamReader
+        sr = New StreamReader(rd)
+        TextBox2.Show()
+        TextBox2.Text = sr.ReadToEnd
+        sr.Close()
+    End Sub
+
+    Private Sub Button12_Click(sender As System.Object, e As System.EventArgs) Handles Button12.Click
+
+        File.Delete(appData + CSM + "\" + Label7.Text + "\" + Label10.Text + ".txt")
+        Dim sw As StreamWriter
+        sw = New StreamWriter(appData + CSM + "\" + Label7.Text + "\" + Label10.Text + ".txt", False)
+        sw.Write(TextBox2.Text)
+        sw.Close()
+        TextBox2.Clear()
+        TextBox2.Hide()
+        TreeView1.Show()
+        fillTree()
+
+
+
+    End Sub
+End Class
